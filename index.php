5d1c093b6b66eb7d3c7f32bc84be06eacc5e4060
JGearhart04 <JGearhart@gmail.com>
2017-02-21 16:44:45 -0800

    Update index.php
    Added deck array.

--- index.php
+++ index.php
@@ -1,5 +1,20 @@
 <?
- $PlayerNames = array("Matthew", "Ester", "Jude", "Ruth");
+$deck = array();
+for ($i=1; $i<53; $i++){
+    $deck[] =$i;
+}
+$shuffle[$deck];
+print_r($deck);
+$card = array_pop($deck);
+
+function displayCardImage(){
+    global $deck;
+    $suitArray = array("clubs", "diamonds", "hearts", "spades");
+    $randomIndex = rand(0,3);
+    $randomSuit = $suitArray[$randomIndex];
+    echo "<img src='img/cards/$randomSuit/".rand(1,13).".png'/>";
+ 
+$PlayerNames = array("Matthew", "Ester", "Jude", "Ruth");
 
 function getHand()
 {
